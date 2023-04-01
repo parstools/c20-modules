73dc4b033589cd5a28408d78fc01075103f3eb5f
Andrzej Borucki <borucki.andrzej@gmail.com>
2023-04-01 10:56:19 +0200

    CMake.g4

--- parser/cmake/ExternalAntlr4Cpp.cmake
+++ parser/cmake/ExternalAntlr4Cpp.cmake
@@ -144,3 +144,44 @@ macro(antlr4cpp_process_grammar
   message(STATUS "Antlr4Cpp ${antlr4cpp_project_namespace} include: ${ANTLR4CPP_GENERATED_SRC_DIR}/${antlr4cpp_project_namespace}")
 
 endmacro()
+
+
+macro(antlr4cpp_process_grammar_one
+    antlr4cpp_project
+    antlr4cpp_project_namespace
+    antlr4cpp_grammar_parser)
+
+  if(EXISTS "${ANTLR4CPP_JAR_LOCATION}")
+    message(STATUS "Found antlr tool: ${ANTLR4CPP_JAR_LOCATION}")
+  else()
+    message(FATAL_ERROR "Unable to find antlr tool. ANTLR4CPP_JAR_LOCATION:${ANTLR4CPP_JAR_LOCATION}")
+  endif()
+
+  add_custom_target("antlr4cpp_generation_${antlr4cpp_project_namespace}"
+    COMMAND
+    ${CMAKE_COMMAND} -E make_directory ${ANTLR4CPP_GENERATED_SRC_DIR}
+    COMMAND
+    "${Java_JAVA_EXECUTABLE}" -jar "${ANTLR4CPP_JAR_LOCATION}" -Werror -Dlanguage=Cpp -listener -visitor -o "${ANTLR4CPP_GENERATED_SRC_DIR}/${antlr4cpp_project_namespace}" -package ${antlr4cpp_project_namespace} "${antlr4cpp_grammar_parser}"
+    WORKING_DIRECTORY "${CMAKE_BINARY_DIR}"
+    DEPENDS "${antlr4cpp_grammar_parser}"
+    )
+
+  # Find all the input files
+  FILE(GLOB generated_files ${ANTLR4CPP_GENERATED_SRC_DIR}/${antlr4cpp_project_namespace}/*.cpp)
+
+  # export generated cpp files into list
+  foreach(generated_file ${generated_files})
+    list(APPEND antlr4cpp_src_files_${antlr4cpp_project_namespace} ${generated_file})
+    set_source_files_properties(
+      ${generated_file}
+      PROPERTIES
+      COMPILE_FLAGS -Wno-overloaded-virtual
+      )
+  endforeach(generated_file)
+  message(STATUS "Antlr4Cpp  ${antlr4cpp_project_namespace} Generated: ${generated_files}")
+
+  # export generated include directory
+  set(antlr4cpp_include_dirs_${antlr4cpp_project_namespace} ${ANTLR4CPP_GENERATED_SRC_DIR}/${antlr4cpp_project_namespace})
+  message(STATUS "Antlr4Cpp ${antlr4cpp_project_namespace} include: ${ANTLR4CPP_GENERATED_SRC_DIR}/${antlr4cpp_project_namespace}")
+
+endmacro()
