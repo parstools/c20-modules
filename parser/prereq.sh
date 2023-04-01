9ffe41cbd669e561e6f04ce41dd97ce3c9fa2539
Andrzej Borucki <borucki.andrzej@gmail.com>
2023-04-01 09:16:51 +0200

    prereq.sh call clear.sh

--- parser/prereq.sh
+++ parser/prereq.sh
@@ -1,9 +1,10 @@
+./clear.sh
 mkdir thirdparty
 mkdir thirdparty/antlr
 cd thirdparty
 cd antlr
 wget -c https://www.antlr.org/download/antlr-4.12.0-complete.jar
-cd ../..
+cd ..
 mkdir cpp_runtime
 cd cpp_runtime
 wget -c https://www.antlr.org/download/antlr4-cpp-runtime-4.12.0-source.zip
